10aab773993c06e57df06f481648fbb045d4cfa5
James Edwards <james@jamesedwards.net>
2022-01-17 19:07:38 -0500

    update user model

--- app/src/models/user.js
+++ app/src/models/user.js
@@ -1,4 +1,5 @@
-const db = require("../db");
+const { knex } = require("../db");
+const { randomBytes, pbkdf2 } = require("crypto");
 
 class UserModel {
   constructor(
@@ -18,19 +19,62 @@ class UserModel {
   ) {
     Object.assign(this, init);
   }
-  create() {}
-  read(cb) {
-    db.query(
-      "SELECT * FROM app_user WHERE id = ?",
-      [this.id],
-      function (err, results) {
-        if (err) throw err;
-        cb(results[0]);
-      }
-    );
+
+  hashPassword(password, cb) {
+    pbkdf2(password, this.salt, 100000, 64, "sha512", (error, derivedKey) => {
+      if (error) throw error;
+      cb(derivedKey.toString("hex"));
+    });
   }
+
+  // todo: base
+  create(password, cb) {
+    randomBytes(64, (error, buff) => {
+      if (error) throw error;
+
+      this.salt = buff.toString("hex");
+
+      this.hashPassword(password, (hash) => {
+        this.hash = hash;
+
+        knex("app_user")
+          .insert(this)
+          .then((result) => {
+            cb({ id: result[0] });
+          });
+      });
+    });
+  }
+  // todo: base
+  read(columns, cb) {
+    knex("app_user")
+      .where(this)
+      .select(columns)
+      .first()
+      .then(function (user) {
+        cb(user);
+      });
+  }
+  // todo: base
   update() {}
+  // todo: base
   delete() {}
+  // todo: base
+  columns() {
+    return {
+      id: "id",
+      email: "email",
+      salt: "salt",
+      hash: "hash",
+      firstName: "first_name",
+      lastName: "last_name",
+      forgotPasswordSalt: "forgot_password_salt",
+      forgotPasswordHash: "forgot_password_hash",
+      forgotPasswordTokenEnd: "forgot_password_token_end",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    };
+  }
 }
 
 module.exports = UserModel;
