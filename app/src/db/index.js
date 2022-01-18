9e8eac300e45c8cf1b45fe119a62c180f3e85de6
James Edwards <james@jamesedwards.net>
2022-01-17 19:00:17 -0500

    include knex

--- app/src/db/index.js
+++ app/src/db/index.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+};
+
+const knex = require("knex")({
+  client: "mysql",
+  connection: dbConfig,
 });
+
+const connection = mysql.createConnection(dbConfig);
+
+const db = { knex, connection };
 
 module.exports = db;
